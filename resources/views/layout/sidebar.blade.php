69405f112c9b07257a24f5299fc78ac21aad591b
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-16 21:04:08 +0700

    fix: sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -4,41 +4,45 @@
 
 <div x-data="{ open: false }" class="relative h-screen" @click.away="open = false">
     <div :class="open ? 'translate-x-0' : '-translate-x-full'"
-        class="fixed z-30 top-0 left-0 h-screen min-w-[120px] bg-white flex flex-col items-center py-24 transform transition-transform duration-300 ease-in-out">
+        class="fixed z-30 top-0 left-0 h-screen min-w-[120px] bg-[#F6F9FA] flex flex-col items-center py-24 transform transition-transform duration-300 ease-in-out rounded-r-3xl shadow-smooth">
         <div class="flex flex-col gap-6">
             <a href="/dashboard">
                 <div class="tooltip tooltip-right" data-tip="Dashboard">
-                    <button class="btn rounded-full w-14 h-14">
+                    <button
+                        class="hover:bg-[#E3ECF1] hover:scale-[102%] flex items-center justify-center duration-300  rounded-full w-14 h-14">
                         <x-lucide-layout-grid class="w-6 h-6" />
                     </button>
                 </div>
             </a>
             <a href="/courses">
                 <div class="tooltip tooltip-right" data-tip="Courses">
-                    <button class="btn rounded-full w-14 h-14">
+                    <button
+                        class="hover:bg-[#E3ECF1] hover:scale-[102%] flex items-center justify-center duration-300  rounded-full w-14 h-14">
                         <x-lucide-laptop class="w-6 h-6" />
                     </button>
                 </div>
             </a>
             <a href="/flashcard">
                 <div class="tooltip tooltip-right" data-tip="Flashcard">
-                    <button class="btn rounded-full w-14 h-14">
+                    <button
+                        class="hover:bg-[#E3ECF1] hover:scale-[102%] flex items-center justify-center duration-300  rounded-full w-14 h-14">
                         <x-lucide-atom class="w-6 h-6" />
                     </button>
                 </div>
             </a>
             <a href="/">
                 <div class="tooltip tooltip-right" data-tip="Dashboard">
-                    <button class="btn rounded-full w-14 h-14">
+                    <button
+                        class="hover:bg-[#E3ECF1] hover:scale-[102%] flex items-center justify-center duration-300  rounded-full w-14 h-14">
                         <x-lucide-file class="w-6 h-6" />
                     </button>
                 </div>
             </a>
         </div>
-        <div class="flex flex-col h-full items-end justify-end">
+        <div class="flex flex-col h-full items-end justify-end gap-5">
             <div class="tooltip tooltip-right" data-tip="Logout">
                 <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                    <button class="btn rounded-full w-14 h-14 bg-white border-none ">
+                    <button class="rounded-full w-14 h-14 bg-none border-none hover:bg-[#E3ECF1] flex items-center justify-center">
                         @csrf
                         <x-lucide-door-open class="w-6 h-6" />
                     </button>
@@ -47,7 +51,6 @@
 
             <a href="/profile">
                 <div class="tooltip tooltip-right" data-tip="Profile">
-                    </button>
                     <div class="w-14 h-14 rounded-full overflow-hidden bg-gray-300">
                         <img class="w-full object-cover"
                             src="
