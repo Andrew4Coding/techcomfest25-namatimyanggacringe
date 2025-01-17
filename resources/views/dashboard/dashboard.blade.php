bacee44c810d5ed322fac30c2655db75b79c3f20
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-01-17 09:13:52 +0700

    fix: dashboard styling

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,19 +1,3 @@
-@php
-    $studyGoals = [
-        (object) [
-            'title' => 'Mengerjakan Tugas 1',
-            'description' => 'Mengerjakan tugas 1 yang diberikan oleh guru',
-        ],
-        (object) [
-            'title' => 'Mengerjakan Tugas 2',
-            'description' => 'Mengerjakan tugas 2 yang diberikan oleh guru',
-        ],
-        (object) [
-            'title' => 'Mengerjakan Tugas 3',
-            'description' => 'Mengerjakan tugas 3 yang diberikan oleh guru',
-        ],
-    ];
-@endphp
 @extends('layout.layout')
 @section('content')
     @if (Auth::user()->userable_type === 'App\Models\Student')
--- resources/views/dashboard/sections/teacher.blade.php
+++ resources/views/dashboard/sections/teacher.blade.php
@@ -43,15 +43,30 @@
                         $theme = config('constants.theme')[$selected_theme];
                     @endphp
                     <a href="/submission/{{ $deadline['id'] }}">
-                        <div class="w-full overflow-hidden shadow-lg rounded-3xl hover:scale-[101%] duration-300 course-{{ $selected_theme }} e"
+                        <div class="w-full relative overflow-hidden shadow-lg rounded-3xl hover:scale-[101%] duration-300 course-{{ $selected_theme }} e"
                             style="background-color: {{ $theme['primary'] }}; color: {{ $theme['secondary'] ?? $theme['secondary'] }}">
+                            @if ($selected_theme == 'sosiologi')
+                                <img src="{{ asset('corner/yellow-corner-left.png') }}" alt=""
+                                    class="absolute bottom-0 left-0 w-40 h-20 object-contain z-0">
+                                <img src="{{ asset('corner/yellow-corner-right.png') }}" alt=""
+                                    class="absolute top-0 right-0 w-40 h-20 object-contain z-0">
+                            @elseif ($selected_theme == 'ekonomi')
+                                <img src="{{ asset('corner/green-corner-left.png') }}" alt=""
+                                    class="absolute bottom-0 -left-12 w-40 h-20 object-contain z-0">
+                                <img src="{{ asset('corner/green-corner-right.png') }}" alt=""
+                                    class="absolute top-0 -right-12 w-40 h-20 object-contain z-0">
+                            @elseif ($selected_theme == 'bahasa')
+                                <img src="{{ asset('corner/blue-corner.png') }}" alt=""
+                                    class="absolute top-0 left-0 object-contain z-0">
+                            @endif
                             <div class="px-6 py-4 flex flex-col justify-between ">
                                 <div class="flex justify-between items-center">
                                     <div class="">
                                         <div class="badge badge-primary font-normal text-sm mb-2 text-white"
                                             style="background-color: {{ $theme['secondary'] }};">
                                             {{ ucfirst($deadline['course']['subject']) }}</div>
-                                        <div class="font-semibold text-lg mb-2" style="color: {{ $theme['tertiary'] }}">
+                                        <div class="font-semibold text-lg mb-2"
+                                            style="color: {{ $theme['tertiary'] }}">
                                             {{ $deadline['courseItem']['name'] }}</div>
                                     </div>
                                     <span
